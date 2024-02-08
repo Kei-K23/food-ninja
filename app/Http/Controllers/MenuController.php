4b8454c060c6177ec290a6ed72a4f8ea3ceb2f2e
Kei-K23 <keiksl2301@gmail.com>
2024-02-08 10:36:04 +0630

    fix image upload type error in menu store

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -30,11 +30,11 @@ class MenuController extends Controller
         // Check if a new image has been uploaded
         if ($request->hasFile('image_url')) {
             // Store the image in the storage
-            $fileName = time() . '.' . $request->image_url->getClientOriginalExtension(); // Get original extension
+            $fileName = time() . '.' . $request->image_url->extension();
             $request->image_url->storeAs('public/images', $fileName);
 
-            // Update the 'image_url' field with the file name only
-            $validatedData['image_url'] = $fileName;
+
+            $validateData['image_url'] = $fileName;
         }
 
         Menu::create($validateData);
